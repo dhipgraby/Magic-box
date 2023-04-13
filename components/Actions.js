ed8c859e9a5495b5d99ac4f14258aa195dc290ef
kennow <kenneth.zambrano4@gmail.com>
2023-04-13 18:05:51 +0200

    fix text size

--- components/Actions.js
+++ components/Actions.js
@@ -1,6 +1,6 @@
 import styles from "../css/Actions.module.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMousePointer, faCompress} from '@fortawesome/free-solid-svg-icons';
+import { faMousePointer, faCompress } from '@fortawesome/free-solid-svg-icons';
 
 export default function Actions() {
 
@@ -8,16 +8,22 @@ export default function Actions() {
         <div className={styles.ul}>
             <div className="row my-3">
                 <div className="col-md-4 col-sm-12 my-3 text-center">
-                <FontAwesomeIcon icon={faMousePointer} /> Click : <br />
-                    <span>Switch cell color</span>
+                    <p>
+                        <FontAwesomeIcon icon={faMousePointer} /> Click : <br />
+                        <span>Switch cell color</span>
+                    </p>
                 </div>
                 <div className="col-md-4 col-sm-12 my-3 text-center">
-                <FontAwesomeIcon icon={faMousePointer} /> <FontAwesomeIcon icon={faMousePointer} />  Double Click:<br />
-                    <span>  Column to cell color</span>
+                    <p>
+                        <FontAwesomeIcon icon={faMousePointer} /> <FontAwesomeIcon icon={faMousePointer} />  Double Click:<br />
+                        <span>  Column to cell color</span>
+                    </p>
                 </div>
                 <div className="col-md-4 col-sm-12 my-3 text-center">
-                <FontAwesomeIcon icon={faCompress} /> Long pressing: <br />
-                    <span> Selected cells to initial color</span>
+                    <p>
+                        <FontAwesomeIcon icon={faCompress} /> Long pressing: <br />
+                        <span> Selected cells to initial color</span>
+                    </p>
                 </div>
             </div>
         </div>
